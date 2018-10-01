4b10a0771ea52f69e1431aee7dd58f3a6f6143e0
Moises Serrano <johndoe@example.com>
2018-10-01 18:56:28 -0400

    anadiendo los datos filtrados a la ui

--- src/entries/home.js
+++ src/entries/home.js
@@ -11,7 +11,8 @@ import reducer from '../reducers/data';
 const initialState = {
     data: {
         ...data
-    }
+    },
+    search: [],
 }
 
 const store = createStore(
--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,12 +1,14 @@
 function data (state, action){
     switch (action.type) {
         case 'SEARCH_VIDEO':{
-             
-            const list = state.data.categories[2].playlist;
-            console.log(list);
-            const results = list.filter( (item)=> {
-                return item.author.includes(action.payload.query)
-            })
+            let results = [];
+            if (action.payload.query) {
+                    const list = state.data.categories[2].playlist;
+                    const results = list.filter( (item)=> {
+                        console.log(item);
+                        return item.author.includes(action.payload.query)
+                    })
+            }
             return {
                 ...state,
                 search: results
