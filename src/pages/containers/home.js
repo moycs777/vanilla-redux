d1e827fa49a9cc30cf42c9535a0c8a83c90bb392
Moises Serrano <johndoe@example.com>
2018-10-03 21:50:38 -0400

    creando acciones

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -8,23 +8,24 @@ import HandleError from '../../error/containers/handle-error';
 import VideoPlayer from '../../player/containers/video-player';
 import { connect } from 'react-redux';
 import { List as list } from 'immutable';
+import { openModal, closeModal } from '../../actions/index';
 
 class Home extends Component {
-
+  // state = {
+  //   modalVisible: false,
+  // }
   handleOpenModal = (id) => {
-    this.props.dispatch({
-      type: 'OPEN_MODAL',
-      payload: {
-        mediaId: id
-      }
-    })
-
+    this.props.dispatch(openModal(id))
+    // this.setState({
+    //   modalVisible: true,
+    //   media
+    // })
   }
   handleCloseModal = (event) => {
-
-    this.props.dispatch({
-      type: 'CLOSE_MODAL'
-    })
+    // this.setState({
+    //   modalVisible: false,
+    // })
+    this.props.dispatch(closeModal())
   }
   render() {
     return (
@@ -45,7 +46,8 @@ class Home extends Component {
                 <VideoPlayer
                   autoplay
                   id={this.props.modal.get('mediaId')}
-
+                  // src={this.state.media.src}
+                  // title={this.state.media.title}
                 />
               </Modal>
             </ModalContainer>
--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Search from '../components/search';
 import { connect } from 'react-redux';
+import { searchEntities } from '../../actions/index';
 
 class SearchContainer extends Component {
   state = {
@@ -8,14 +9,8 @@ class SearchContainer extends Component {
   }
   handleSubmit = event => {
     event.preventDefault();
-    console.log(this.input.value, 'submit value');
-    this.props.dispatch({
-      type:'SEARCH_VIDEO',
-      payload:{
-        query: this.input.value
-      }
-    })
-
+    // console.log(this.input.value, 'submit')
+    this.props.dispatch(searchEntities(this.input.value))
   }
   setInputRef = element => {
     this.input = element;
@@ -37,4 +32,4 @@ class SearchContainer extends Component {
   }
 }
 
-export default connect() (SearchContainer);
+export default connect()(SearchContainer);
