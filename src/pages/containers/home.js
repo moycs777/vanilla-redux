ef499c0f7a7578e13d0fd6d6514a63ba3f991c9e
Moises Serrano <johndoe@example.com>
2018-10-03 20:45:07 -0400

    Actualizando un estado inmutable

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import HomeLayout from '../components/home-layout';
 import Categories from '../../categories/components/categories';
 import Related from '../components/related';
@@ -8,9 +6,8 @@ import ModalContainer from '../../widgets/containers/modal';
 import Modal from '../../widgets/components/modal';
 import HandleError from '../../error/containers/handle-error';
 import VideoPlayer from '../../player/containers/video-player';
-import {
-  connect
-} from 'react-redux';
+import { connect } from 'react-redux';
+import { List as list } from 'immutable';
 
 class Home extends Component {
   state = {
@@ -28,36 +25,30 @@ class Home extends Component {
     })
   }
   render() {
-    return ( <HandleError >
-      <HomeLayout >
-      <Related / >
-      <Categories categories = {
-        this.props.categories
-      }
-      handleOpenModal = {
-        this.handleOpenModal
-      }
-      search = {
-        this.props.search
-      }
-      /> {
-        this.state.modalVisible &&
-          <ModalContainer >
-          <Modal
-        handleClick = {
-            this.handleCloseModal
-          } >
-          <VideoPlayer
-        autoplay
-        src = {
-          this.state.media.src
-        }
-        title = {
-          this.state.media.title
-        }
-        /> 
-        </Modal> </ModalContainer>
-      } </HomeLayout> 
+    return (
+      <HandleError>
+        <HomeLayout>
+          <Related />
+          <Categories
+            categories={this.props.categories}
+            handleOpenModal={this.handleOpenModal}
+            search={this.props.search}
+          />
+          {
+            this.state.modalVisible &&
+            <ModalContainer>
+              <Modal
+                handleClick={this.handleCloseModal}
+              >
+                <VideoPlayer
+                  autoplay
+                  src={this.state.media.src}
+                  title={this.state.media.title}
+                />
+              </Modal>
+            </ModalContainer>
+          }
+        </HomeLayout>
       </HandleError>
     )
   }
@@ -67,10 +58,17 @@ function mapStateToProps(state, props) {
   const categories = state.get('data').get('categories').map((categoryId) => {
     return state.get('data').get('entities').get('categories').get(categoryId)
   })
-
+  let searchResults = list()
+  const search = state.get('data').get('search');
+  if (search) {
+    const mediaList = state.get('data').get('entities').get('media');
+    searchResults = mediaList.filter((item) => (
+      item.get('author').toLowerCase().includes(search.toLowerCase())
+    )).toList();
+  }
   return {
     categories: categories,
-    search: state.get('data').get('search')
+    search: searchResults
   }
 
 }
--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,36 +1,22 @@
 import schema from '../schemas/index.js';
-import {
-    fromJS
-} from 'immutable';
+import { fromJS } from 'immutable';
 
 const initialState = fromJS({
-    entities: schema.entities,
-    categories: schema.result.categories,
-    search: [],
+  entities: schema.entities,
+  categories: schema.result.categories,
+  search: '',
 })
 
 
 
 function data(state = initialState, action) {
-    switch (action.type) {
-        case 'SEARCH_VIDEO':
-            {
-                // action.payload.query
-                let results = [];
-                if (action.payload.query) {
-                    const list = state.data.categories[2].playlist;
-                    results = list.filter((item) => {
-                        return item.author.includes(action.payload.query)
-                    })
-                }
-                return {
-                    ...state,
-                    search: results
-                }
-            }
-        default:
-            return state
+  switch (action.type) {
+    case 'SEARCH_VIDEO': {
+      return state.set('search', action.payload.query)
     }
+    default:
+      return state
+  }
 }
 
 export default data;
