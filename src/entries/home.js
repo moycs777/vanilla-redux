5aebe174c4393a19e03932884a0966b0a2fd3bb4
Moises Serrano <johndoe@example.com>
2018-10-05 11:58:00 -0400

    usando middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,54 +3,44 @@ import {
     render
 } from 'react-dom';
 import Home from '../pages/containers/home';
-// import Playlist from './src/playlist/components/playlist';
-// import data from '../api.json';
-// console.log('Hola mundo!' )
-// import data from '../schemas/index.js';
-
 import {
     Provider
 } from 'react-redux';
-
 import {
-    createStore
+    createStore,
+    applyMiddleware
 } from 'redux';
 import reducer from '../reducers/index';
 import {
     Map as map
 } from 'immutable';
 
-// console.log(data);
-// console.log(data);
-
-// const initialState = {
-//   data: {
-//     // ...data,
-//     entities: data.entities,
-//     categories: data.result.categories,
-//     search: [],
-//   },
-//   modal: {
-//     visibility: false,
-//     mediaId: null,
-//   }
-// }
+const logger = ({
+    getState,
+    dispatch
+}) => next => action => {
+    console.log('este es mi viejo estado', getState().toJS())
+    console.log('vamos a enviar está acción', action);
+    const value = next(action)
+    console.log('este es mi nuevo estado', getState().toJS())
+    return value
+}
 
 const store = createStore(
     reducer,
     map(),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(logger)
 );
 
-// console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
-// ReactDOM.render(que voy a renderizar, donde lo haré);
-// const holaMundo = <h1>hola Estudiante!</h1>;
 
-render( <Provider store = {
+render( <
+    Provider store = {
         store
     } >
-    <Home / >
-    </Provider>, homeContainer);
+    <
+    Home / >
+    <
+    /Provider>, homeContainer);
